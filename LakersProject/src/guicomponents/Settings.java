d65e7a47bd82a422e4967b5e6cd7f4e7303c68e7
gargi79 <49700650+gargi79@users.noreply.github.com>
2019-05-28 16:01:19 -0700

    Create Settings.java

---
+++ LakersProject/src/guicomponents/Settings.java
@@ -0,0 +1,41 @@
+package guicomponents;
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Settings extends JFrame {
+
+String userWord = "";
+JTextField userInput;
+
+public Settings() {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JLabel lable = new JLabel(" ");
+JButton jButton = new JButton(" ");
+userInput = new JTextField("", 30);
+jButton.addActionListener( (e) -> {
+    Action();
+});
+frame.setSize(300, 300);
+//frame.setVisible(true);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.add(panel);
+panel.add(lable);
+panel.add(userInput);
+panel.add(jButton);
+}
+
+private void Action() {
+userWord = userInput.getText();
+
+}
+
+public static void main(String[] args) {
+new Settings();
+}
+}
