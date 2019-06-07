21a03fcea54189888a003dd822c8d3ada0004e16
DWLooney <DWLooney@10.16.13.70>
2019-06-06 19:30:25 -0700

    Add JavaDocs for data package and test cases for Project class. (WIP)

--- LakersProject/src/data/Database.java
+++ LakersProject/src/data/Database.java
@@ -3,17 +3,36 @@ package data;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Stores all of the projects in the program.
+ * Last Modified: 6 June 2019
+ * @author Daniel Looney (For entire class)
+ */
 public class Database {
+	
+	/** Stores all of the Projects. */
 	private List<Project> myProjects;
 	
+	/**
+	 * Constructs the database.
+	 */
 	public Database() {
 		myProjects = new ArrayList<Project>();
 	}
 	
+	/**
+	 * Adds a project to the database
+	 * @param proj Project to be added to the database
+	 */
 	public void addProject(Project proj) {
 		myProjects.add(proj);
 	}
 	
+	/**
+	 * Gets a project based of it's name.
+	 * @param projName Name of project to get.
+	 * @return Found project in database.
+	 */
 	public Project getProject(String projName) {
 		Project curr = null;
 		for (Project p : myProjects) {
@@ -24,14 +43,19 @@ public class Database {
 		return curr;
 	}
 	
+	/**
+	 * Gets a list of all projects the database.
+	 * @return List of all projects in the database.
+	 */
 	public List<Project> getAllProjects(){
 		return myProjects;
 	}
 	
+	//TODO
 	public void sortByDifficulty(String diff) {
 		
 	}
-	
+	//TODO
 	public void sortByCost(int low, int high) {
 		
 	}
--- LakersProject/src/data/ProjItems.java
+++ LakersProject/src/data/ProjItems.java
@@ -3,51 +3,73 @@ package data;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Data representation of the project time span.
+ * Last Modified: 06 June 2019
+ * @author Daniel Looney (Whole Class)
+ *
+ */
 public class ProjItems {
+	
+	/** Project materials */
 	private List<String> myMaterials;
+	
+	/** Project Tools */
 	private List<String> myTools;
 	
+	/**
+	 * Constructs the lists representation materials and tools.
+	 */
 	public ProjItems() {
 		myMaterials = new ArrayList<String>();
 		myTools = new ArrayList<String>();
 	}
 	
+	/**Adds a material to the project. */
 	public void addMat(String material) {
 		myMaterials.add(material);
 	}
 	
+	/** Removes a material from the project. */
 	public void removeMat(String material) {
 		if (myMaterials.contains(material)) {
 			myMaterials.remove(material);
 		}
 	}
 	
+	/** Clears all materials in the project. */
 	public void clearMat() {
 		myMaterials.clear();
 	}
 	
+	/** Gets a list of materials in the project. */
 	public List<String> getMat() {
 		return myMaterials;
 	}
 	
+	/** Adds a tool to the project. */
 	public void addTool(String tool) {
 		myTools.add(tool);
 	}
 	
+	/** Removes a tool from the project. */
 	public void removeTool(String tool) {
 		if (myTools.contains(tool)) {
 			myTools.remove(tool);
 		}
 	}
 	
+	/** Clears all tools in the project. */
 	public void clearTool() {
 		myTools.clear();
 	}
 	
+	/** Gets a list of tools in the project. */
 	public List<String> getTools() {
 		return myTools;
 	}
 	
+	/** String representation of the project items. */
 	public String toString() {
 		String output = "\"";
 		for (String temp : myMaterials) {
